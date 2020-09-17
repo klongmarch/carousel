9b2f3f940cb4e1a9ea62b9cc342798c2acea9037
steve kang <klongmarch@yahoo.com>
2020-09-17 12:19:55 -0500

    Updates

---
+++ precache-manifest.e421b112e054a1a6a3ad448b0396016b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e48cb6e0c63d528fea4d94b8b3c7e17",
+    "url": "/carousel/index.html"
+  },
+  {
+    "revision": "91ef500d565c1ca9af0b",
+    "url": "/carousel/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "6026a767e1d541c1d41c",
+    "url": "/carousel/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "91ef500d565c1ca9af0b",
+    "url": "/carousel/static/js/2.93e32125.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/carousel/static/js/2.93e32125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6026a767e1d541c1d41c",
+    "url": "/carousel/static/js/main.1f10a8f2.chunk.js"
+  },
+  {
+    "revision": "2334337016e041c9c1cf",
+    "url": "/carousel/static/js/runtime-main.9b8b9a58.js"
+  }
+]);
